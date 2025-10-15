13562cc4c26982afdb3979659842f53d5d2991f8
Cucumberi135 <action.ytert@gmail.com>
2025-10-15 12:11:09 +0300

    added supabase

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://vzwbfcndlanfgyxcdmjv.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6d2JmY25kbGFuZmd5eGNkbWp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA0NjU4MDYsImV4cCI6MjA3NjA0MTgwNn0.ouWwcALKAB3qBksqoLdDfsJcMUc7t_tIorDvbfkGrsk',
+  );
+
   runApp(const MyApp());
 }
 
@@ -10,11 +18,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Supabase Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Supabase Flutter Demo'),
     );
   }
 }
@@ -31,28 +39,41 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  @override
+  void initState() {
+    super.initState();
+    testSupabase(); // ✅ теперь метод в правильном месте (внутри State)
+  }
+
+  // Проверка соединения с Supabase
+  Future<void> testSupabase() async {
+    try {
+      final res =
+          await Supabase.instance.client.from('todos').select().limit(1);
+      print('Соединение работает ✅: $res');
+    } catch (e) {
+      print('Ошибка соединения ❌: $e');
+    }
+  }
+
   void _incrementCounter() {
     setState(() {
+      _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
         title: Text(widget.title),
       ),
       body: Center(
-
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            const Text('Ты нажал кнопку столько раз:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
@@ -62,9 +83,9 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        tooltip: 'Увеличить',
         child: const Icon(Icons.add),
-      ), 
+      ),
     );
   }
 }
